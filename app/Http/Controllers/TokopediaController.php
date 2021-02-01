a358b98501eb9e7bd7378be3db6c382a0407088a
ramdanriawan <ramdanriawan3@gmail.com>
2021-02-01 20:31:01 +0700

    4

--- app/Http/Controllers/TokopediaController.php
+++ app/Http/Controllers/TokopediaController.php
@@ -12,30 +12,34 @@ class TokopediaController extends Controller
 {
     public function index()
     {
-        $folderCekGambar  = glob('C:\Users\ramdan3mts\Documents\ubah-sekaligus-gambar\271386\*.xlsx');
-        $folderUbahStatus = glob('C:\Users\ramdan3mts\Documents\ubah-sekaligus-status\271385\*.xlsx');
+        
+        $folderCekGambar  = glob(public_path('tokopedia\file_lama\271386\*.xlsx'));
+        $folderUbahStatus = glob(public_path('tokopedia\file_lama\271385\*.xlsx'));
 
-        // array_shift($folderCekGambar);
-        // array_shift($folderUbahStatus);
+        // untuk membuat open link in new tab javascript
+        if(!request()->file_ke && !request()->baris_ke) {
+            for ($i=$_GET['start_dari']; $i < $_GET['berhenti_di']; $i++) { 
+                
+                echo "<script>Window.open(\"?file_ke=$i&baris_ke=4\", \"_blank\"); </script>";
+            }
+
+            return false;
+        }
 
         $folderKeBerapa = 1;
-        foreach ($folderCekGambar as $keyFolder => $folderItem) { 
-
-            // untuk skip karena sering gagal, jadi biar gk ngulang lagi gannnn
-            // if($folderKeBerapa <= 1) continue;
-
-            $file_excel = $folderItem;
+        for ($a = $_GET['file_ke']; $a < count($folderCekGambar); $a++) { 
+            $file_excel = $folderCekGambar[$a];
             $worksheet  = \PHPExcel_IOFactory::createReaderForFile($file_excel)->load($file_excel)->getSheet(0);
 
             // update produk
-            $excel2 = \PHPExcel_IOFactory::createReader(\PHPExcel_IOFactory::identify($folderUbahStatus[$keyFolder]));
-            $excel2 = $excel2->load($folderUbahStatus[$keyFolder]);
+            $excel2 = \PHPExcel_IOFactory::createReader(\PHPExcel_IOFactory::identify($folderUbahStatus[$a]));
+            $excel2 = $excel2->load($folderUbahStatus[$a]);
             $excel2->setActiveSheetIndex(0);
 
             $gambar = [];
             $fileSaveName = 'tokopedia/file_update/TOKOPEDIA_PRODUCT_UPDATE_' . time() . '.xlsx';
-            // $fileSaveName = 'TOKOPEDIA_PRODUCT_UPDATE_1612142048.xlsx';
-            for ($row = 0; $row <= $worksheet->getHighestRow(); ++$row) {
+            
+            for ($row = $_GET['baris_ke']; $row <= $worksheet->getHighestRow(); ++$row) {
                 $nomor = $row - 3;
 
                 echo "Folder ke $folderKeBerapa, item ke $nomor <br>";
@@ -88,7 +92,7 @@ class TokopediaController extends Controller
                 }
 
                 if($gambarIniGakAdaWatermark != 1) {
-                    echo "Ketemu nih produk yg gak ada watermarknya, mantulll <br>";
+                    echo "Ketemu nih produk yg ada watermarknya, mantulll <br>";
 
                     $excel2->getActiveSheet()->setCellValue("I$row", 'Nonaktif');
                 }
